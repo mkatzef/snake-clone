29424f60c14ec2b8467f5bae2134037f6aaca358
Marc Katzef <marckatzef@gmail.com>
2017-07-12 17:46:51 +1200

    Improved documentation, removed some magic numbers.

--- snake-clone.ino
+++ snake-clone.ino
@@ -1,27 +1,46 @@
+/* 
+ * snake-clone.ino
+ * 
+ * A clone of the classic game "Snake" for an Arduino Nano. Controlled through
+ * serial communication.
+ * 
+ * Written by Marc Katzef
+ */
+
+#include <stdlib.h>
+#include <stdbool.h>
+
 //Libraries for Dot Matrix
 #include <SPI.h>
 #include <DMD.h>
 #include <TimerOne.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
+#define COLUMNS 32
+#define ROWS 16
+
+#define SCAN_PERIOD_MICROS 500
+#define BRIGHTNESS_STEPS 2 // Number of screen scans per PWM period
+#define BRIGHTNESS 1 // (Brightness %) * BRIGHTNESS_STEPS
+
+// Controls, based on number pad
+#define DIRECTION_CHAR_UP '8'
+#define DIRECTION_CHAR_RIGHT '6'
+#define DIRECTION_CHAR_DOWN '2'
+#define DIRECTION_CHAR_LEFT '4'
+
+#define DEFAULT_SNAKE_LENGTH 3
+#define GAME_STEP_PERIOD_MILLIS 100
+
+enum directions {DIR_DOWN=0, DIR_LEFT, DIR_UP, DIR_RIGHT};
 
 DMD dmd(1, 1);
 
-#define SPEAKERPIN 4
-#define BUTTON1PIN 2
-#define BUTTON2PIN 3
-#define COLUMNS 32
-#define ROWS 16
-#define BSTEPS 7
+int brightnessCount = 0;   // Keeps track of brightness PWM phase
 
-int brightness=3;   //display brightness
-int bcount=0;   //variable used to keep track of pwm phase
-
-/** Stores the state of the screen. **/
-//unsigned short bitmap[COLUMNS] = {0};
-
-/** Used for every pixel of the snake, and the fly **/
+/* 
+ * The basic component of a linked list storing locations on the display. Used to
+ * represent each pixel of the snake, and the fly.
+ */
 typedef struct segment_s Segment;
 struct segment_s {
   int row;
@@ -30,96 +49,116 @@ struct segment_s {
 };
 
 
-/** Holds a pointer to the first segment of its body, the fly, and its
- * current direction. **/
-typedef struct snek_s Snake;
-struct snek_s {
-  Segment* tail; //The HEAD of a linked list... sorry.
+/* 
+ * A structure to represent the current state of the snake. Stores a pointer to the
+ * first segment of the snake's body, the fly, and the snake's current direction.
+ */
+typedef struct snake_s Snake;
+struct snake_s {
+  Segment* tail; // The HEAD of a linked list... sorry.
   Segment* fly;
   int direction;
 };
 
 
-/** Used in the search for a free pixel, could instead use a single byte
- * to represent a pixel of the display, but this is a bit more
- * readable. **/
-typedef struct tuple_s Tuple;
-struct tuple_s {
-  int row;
-  int col;
-};
-
-
-
-void ScanDMD(){       //also implements a basic pwm brightness control
+/* 
+ * Scans the display, temporarily enabling each DMD pixel written HIGH. Also
+ * implements a simple PWM-based brightness control scheme. Supplies additional
+ * power to display for a number of cycles every period.
+ * Should be called very frequently, perhaps through timer interrupt.
+ */
+void scanDmd() {
   dmd.scanDisplayBySPI();
-  bcount=bcount+brightness;
-  if(bcount<BSTEPS){digitalWrite(9,LOW);}
-  bcount=bcount%BSTEPS;
+  brightnessCount++;
+  
+  if(brightnessCount < BRIGHTNESS) {
+    digitalWrite(9, HIGH);
+  } else {
+    digitalWrite(9, LOW);
+  }
+  
+  if (brightnessCount >= BRIGHTNESS_STEPS) {
+    brightnessCount = 0;
+  }
 }
 
+
+/* 
+ * Initializes required display and communication features.
+ */
 void setup(){
-  Timer1.initialize( 500 );           //period in microseconds to call ScanDMD. Anything longer than 5000 (5ms) and you can see flicker.
-  Timer1.attachInterrupt( ScanDMD );   //attach the Timer1 interrupt to ScanDMD which goes to dmd.scanDisplayBySPI()
-  dmd.clearScreen( true );     //clear/init the DMD pixels held in RAM
-  pinMode(BUTTON1PIN,INPUT_PULLUP);
-  pinMode(BUTTON2PIN,INPUT_PULLUP);
-  pinMode(SPEAKERPIN,OUTPUT);
-  digitalWrite(SPEAKERPIN,LOW);    //speaker off
+  Timer1.initialize(SCAN_PERIOD_MICROS);
+  Timer1.attachInterrupt(scanDmd);
+  dmd.clearScreen(true);
+
   Serial.begin(9600);
 }
 
 
+/* 
+ * Initializes snake and fly randomly on display before entering infinite game
+ * loop. In infinite loop, snake may be controlled by sending one of the expected
+ * DIRECTION_CHAR_X symbols through serial.
+ */
 void loop(){
-  dmd.clearScreen( true );
+  dmd.clearScreen(true);
   
-  Serial.println("1");delay(50);
-  Segment sneks_fly = {0, 0, 0}; //Placeholder
-  Serial.println("2");delay(50);
-  Snake snek = {0, &sneks_fly, rand()%4}; //Uses placeholder, and random direction
-  Serial.println("3");delay(50);
-  new_fly(&snek); //Puts the fly in a random spot
-  Serial.println("4");delay(50);
-  Segment temp = {sneks_fly.row, sneks_fly.col, 0}; //Save it as a separate segment
-  Serial.println("5");delay(50);
-  snek.tail = &temp; //Uses that segment as the first segment
-  Serial.println("6");delay(50);
-  new_fly(&snek); //Gets a new fly for real.
-  Serial.println("7");delay(50);
+  Segment tempFly = {0, 0, 0};                      // Placeholder fly
+  Snake snake = {0, &tempFly, rand() % 4};          // Create new snake using fly and random direction
+  newFly(&snake);                                   // Put the placeholder fly in a random spot
+  Segment tempTail = {tempFly.row, tempFly.col, 0}; // Save it as a separate segment
+  snake.tail = &tempTail;                           // Use that segment as the first segment
+  newFly(&snake);                                   // Get a new fly again
   
-  update_bitmap(&snek);
-  Serial.println("8");delay(50);
-  //updateDisplay();
-  delay(250);
+  updateBitmap(&snake);
   
-  int start_length = 3; //Start length in segments
-  byte dir = 0;
+  int initLengthCounter = DEFAULT_SNAKE_LENGTH; // Number of times snake must grow to reach default length
     
   while (1) {
-    if (start_length) {
-      start_length--;
+    if (initLengthCounter > 0) {
+      initLengthCounter--;
     }
-    
-    int trial = Serial.read() - '0';
-    if (trial == 4) {
-      dir = 1;
-      Serial.println("Left");
-    } else if (trial == 8) {
-      dir = 2;
-      Serial.println("Up");
-    } else if (trial == 6) {
-      dir = 3;
-      Serial.println("Right");
-    } else if (trial == 2) {
-      dir = 0;
-      Serial.println("Down");
+
+    if (Serial.available()) {
+      char inputDir = Serial.read();
+      int newDirection = -1;
+      
+      switch(inputDir) {
+      case DIRECTION_CHAR_LEFT:
+        newDirection = DIR_LEFT;
+        Serial.println("Left");
+        break;
+      case DIRECTION_CHAR_UP:
+        newDirection = DIR_UP;
+        Serial.println("Up");
+        break;
+      case DIRECTION_CHAR_RIGHT:
+        newDirection = DIR_RIGHT;
+        Serial.println("Right");
+        break;
+      case DIRECTION_CHAR_DOWN:
+        newDirection = DIR_DOWN;
+        Serial.println("Down");
+        break;
+      default:
+        Serial.print("Unrecognized char: \"");
+        Serial.print(inputDir);
+        Serial.println('"');
+      }
+      
+      if (newDirection != -1) {
+        if ((snake.direction - newDirection) % 2 == 0) {
+          Serial.println("Ignored");
+        } else {
+          snake.direction = newDirection;
+        }
+      }
     }
       
-    int status = game_step(&snek, dir, start_length);
+    int status = gameStep(&snake, initLengthCounter);
     
-    update_bitmap(&snek);
-    //updateDisplay();
-    delay(100);
+    updateBitmap(&snake);
+    delay(GAME_STEP_PERIOD_MILLIS);
     
     if (status != 0) {
       break;
@@ -128,18 +167,14 @@ void loop(){
 }
 
 
-
-
-
-
 /** Finds a free pixel by randomly drawing a tuple struct from those
  * that do not hold a segment of the given snake. This updates the fly
  * associated with the given snake. If it is successful, it returns 0,
  * otherwise 1. **/
-int new_fly(Snake* snek) {
+int newFly(Snake* snake) {
   unsigned short covered[COLUMNS] = {0};
   
-  Segment* focus = snek->tail;
+  Segment* focus = snake->tail;
   while (focus) {
     covered[focus->col] |= (1 << focus->row);
     focus = focus->next;
@@ -173,8 +208,8 @@ int new_fly(Snake* snek) {
   
   byte row = uncoveredRows[rand() % counter];
   
-  snek->fly->row = row;
-  snek->fly->col = col;
+  snake->fly->row = row;
+  snake->fly->col = col;
   
   return 0;
 }
@@ -203,13 +238,13 @@ int collision(Segment* tail, Segment* head) {
  * Otherwise all is well and returns 0. 
  * Should update for nav_direction to never be -1, instead always the
  * current direction. **/
-int game_step(Snake* snek, int nav_direction, bool starting) {
+int gameStep(Snake* snake, bool starting) {
   //Save tail data in case of length increase
-  int tail_row = snek->tail->row;
-  int tail_col = snek->tail->col;
+  int tail_row = snake->tail->row;
+  int tail_col = snake->tail->col;
   
   //Shift snake segments
-  Segment* focus_seg = snek->tail;
+  Segment* focus_seg = snake->tail;
   Segment* next_seg = focus_seg->next;
   while (next_seg) {
     focus_seg->row = next_seg->row;
@@ -219,15 +254,8 @@ int game_step(Snake* snek, int nav_direction, bool starting) {
   }
   Segment* head = focus_seg;
   
-  //Change direction - probably doesn't need to be in this function... (before instead)
-  if (nav_direction != -1) { //Used to use only one button, so either pressed or not... Update mentioned in function documentation
-    if ((snek->direction - nav_direction) % 2 != 0) {
-      snek->direction = nav_direction;
-    }
-  }
-  
   //Step forward
-  switch (snek->direction) {
+  switch (snake->direction) {
     case 0:
     head->row = (head->row+ROWS-1) % ROWS;
     break; 
@@ -246,16 +274,16 @@ int game_step(Snake* snek, int nav_direction, bool starting) {
   }
   
   //Need to grow
-  if (((head->row == snek->fly->row) && (head->col == snek->fly->col)) || starting) {
+  if (((head->row == snake->fly->row) && (head->col == snake->fly->col)) || starting) {
     
-    Segment* old_tail = malloc(sizeof(Segment)); 
+    Segment* old_tail = malloc(sizeof(Segment)); // WARNING: use of malloc without corresponding free anywhere.
     old_tail->row = tail_row;
     old_tail->col = tail_col;
-    old_tail->next = snek->tail;
-    snek->tail = old_tail;
+    old_tail->next = snake->tail;
+    snake->tail = old_tail;
     
-    if (((head->row == snek->fly->row) && (head->col == snek->fly->col))) { //Ate a fly
-      int status = new_fly(snek);
+    if (((head->row == snake->fly->row) && (head->col == snake->fly->col))) { //Ate a fly
+      int status = newFly(snake);
       if (status != 0) {
         return 1;
       }
@@ -265,7 +293,7 @@ int game_step(Snake* snek, int nav_direction, bool starting) {
   }
   
   //Check for collisions
-  if (collision(snek->tail, head)) {
+  if (collision(snake->tail, head)) {
     return 1;
   }
   
@@ -275,20 +303,20 @@ int game_step(Snake* snek, int nav_direction, bool starting) {
 
 /** Puts a 1 in every position in the bitmask where a snake segment or a
  * fly should be represented. **/
-void update_bitmap(Snake* snek) {
+void updateBitmap(Snake* snake) {
   //for (int i = 0; i < COLUMNS; i++) {
   //  bitmap[i] = 0;
   //}
   
-  Segment* focus_seg = snek->tail;
+  Segment* focus_seg = snake->tail;
   do {
     //bitmap[focus_seg->col] |= 1 << focus_seg->row;
     
     dmd.writePixel(focus_seg->col,focus_seg->row,GRAPHICS_NORMAL,1);
     focus_seg = focus_seg->next;
   } while (focus_seg);
-  //bitmap[snek->fly->col] |= 1 << snek->fly->row;
-    dmd.writePixel(snek->fly->col,snek->fly->row,GRAPHICS_NORMAL,1);
+  //bitmap[snake->fly->col] |= 1 << snake->fly->row;
+    dmd.writePixel(snake->fly->col,snake->fly->row,GRAPHICS_NORMAL,1);
 }
 
 
